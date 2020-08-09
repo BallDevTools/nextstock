c0133d0fd82bc32114ea961a9060194116cfcfaa
ball <56023125+Pitipong879@users.noreply.github.com>
2020-08-09 21:15:46 +0700

    Add files via upload

---
+++ pages/report/typets.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement, useContext } from 'react'
+
+interface Props {
+    
+}
+
+export default function TypesActiions({}: Props): ReactElement {
+    return (
+        <div>
+            <style jsx global>
+                `.container: 
+                <z-index>;500</z-index>`
+            </style>
+        </div>
+    )
+}
